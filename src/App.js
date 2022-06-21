20487871a844a2ac4d45a72c6bf10b984b44c3ee
Alexis Cesar <alexis.cesar2017@outlook.com>
2022-06-21 14:30:28 -0300

    Added routing and moved firebase folder

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import './App.css';
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
+// Components
 import Navbar from './components/Navbar';
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+
+// Pages
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Navbar />
+        <div className="container">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />}/>
+            <Route path="/about" element={<About />}/>
+          </Routes>
+        </div>
       </BrowserRouter>
     </div>
   );
