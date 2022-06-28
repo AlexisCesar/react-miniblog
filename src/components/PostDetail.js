1f3117f9d228498203bbffa1344c3ebf84e5a5d5
Alexis Cesar <alexis.cesar2017@outlook.com>
2022-06-28 11:16:55 -0300

    Changed author location

--- src/components/PostDetail.js
+++ src/components/PostDetail.js
@@ -6,13 +6,13 @@ const PostDetail = ({ post }) => {
     return (
         <div className={styles.postDetail}>
             <h2>{post.title}</h2>
+            <p className={styles.createdBy}>{post.createdBy}</p>
             <img src={post.image} alt={post.title} />
             <div className={styles.tags}>
                 {post.tagsArray.map((tag, index) => (
                     <p key={index}><span>#</span>{tag}</p>
                 ))}
             </div>
-            <p className={styles.createdBy}>{post.createdBy}</p>
             <Link to={`/posts/${post.id}`} className='btn btn-outline'>Read Post</Link>
         </div>
     )
